d57bc9c33d0eb978b8b9dc70411b0c777dd0f4fc
hamzahsn <amdouni.hamza@hotmail.fr>
2021-02-08 23:14:42 +0100

    Add authentication routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AuthenticationComponent } from './authentication/authentication.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthenticationComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
