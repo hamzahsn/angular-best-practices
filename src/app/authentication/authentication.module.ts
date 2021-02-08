7976e016bdfcf7c15136c868e92ec4f671a81b49
hamzahsn <amdouni.hamza@hotmail.fr>
2021-02-08 23:18:49 +0100

    Generate login-form component

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthenticationComponent } from './authentication.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 
 
 @NgModule({
-  declarations: [AuthenticationComponent],
+  declarations: [AuthenticationComponent, LoginFormComponent],
   imports: [
     CommonModule
   ]
