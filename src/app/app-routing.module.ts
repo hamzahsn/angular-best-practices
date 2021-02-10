c17b4b8aac1f7984334608a20c47631431943a25
hamzahsn <amdouni.hamza@hotmail.fr>
2021-02-10 22:44:51 +0100

    Add shipments routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AuthenticationComponent } from './authentication/authentication.component';
+import { ShipmentsComponent } from './shipments/shipments.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AuthenticationComponent,
+  },
+  {
+    path: 'shipments',
+    component: ShipmentsComponent,
   },
 ];
 
