ec0ffcf949f35ef396443e967beb52affe1984e1
hamzahsn <amdouni.hamza@hotmail.fr>
2021-02-10 22:43:36 +0100

    Generate shipments module

---
+++ src/app/shipments/shipments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shipments',
+  templateUrl: './shipments.component.html',
+  styleUrls: ['./shipments.component.scss']
+})
+export class ShipmentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shipments/shipments.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShipmentsComponent } from './shipments.component';
+
+
+
+@NgModule({
+  declarations: [ShipmentsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ShipmentsModule { }
