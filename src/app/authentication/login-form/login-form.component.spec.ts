2f063ba154d05e1205d36af3266e9bf8275d19eb
hamzahsn <amdouni.hamza@hotmail.fr>
2021-02-10 01:48:04 +0100

    Add form validations
    Check if the email and password are required and valid.

--- src/app/authentication/login-form/login-form.component.spec.ts
+++ src/app/authentication/login-form/login-form.component.spec.ts
@@ -1,3 +1,4 @@
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginFormComponent } from './login-form.component';
@@ -5,21 +6,103 @@ import { LoginFormComponent } from './login-form.component';
 describe('LoginFormComponent', () => {
   let component: LoginFormComponent;
   let fixture: ComponentFixture<LoginFormComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginFormComponent ]
-    })
-    .compileComponents();
+      declarations: [LoginFormComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginFormComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('email should be required and match email pattern', () => {
+    const emailControl = component.loginForm.controls.email;
+
+    emailControl.setValue('test');
+    let emailErrors = emailControl.errors || {};
+
+    expect(emailControl.valid).toBeFalsy();
+    expect(emailErrors.pattern).toBeTruthy();
+    expect(emailErrors.pattern.requiredPattern).toEqual(
+      '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'
+    );
+
+    emailControl.setValue('');
+    emailErrors = emailControl.errors || {};
+
+    expect(emailControl.valid).toBeFalsy();
+    expect(emailErrors.required).toBeTruthy();
+  });
+
+  it('password should be required', () => {
+    const passwordControl = component.loginForm.controls.password;
+
+    passwordControl.setValue('');
+    const emailErrors = passwordControl.errors || {};
+
+    expect(passwordControl.valid).toBeFalsy();
+    expect(emailErrors.required).toBeTruthy();
+  });
+
+  it('should display an error message when the email is not valid', () => {
+    const emailControl = component.loginForm.controls.email;
+
+    emailControl.setValue('test');
+    fixture.detectChanges();
+    let errors = debugElement.nativeElement.querySelectorAll('mat-error');
+
+    expect(emailControl.valid).toBeFalsy();
+    expect(errors[0].innerText).toEqual('Email is not valid.');
+
+    emailControl.setValue('');
+    fixture.detectChanges();
+    errors = debugElement.nativeElement.querySelectorAll('mat-error');
+
+    expect(emailControl.valid).toBeFalsy();
+    expect(errors[0].innerText).toEqual('Email is required.');
+  });
+
+  it('should display an error message when the password is not valid', () => {
+    const passwordControl = component.loginForm.controls.password;
+
+    passwordControl.setValue('');
+    fixture.detectChanges();
+    const errors = debugElement.nativeElement.querySelectorAll('mat-error');
+
+    expect(passwordControl.valid).toBeFalsy();
+    expect(errors[1].innerText).toEqual('Password is required.');
+  });
+
+  it('should disable submit button when loginForm is not valid', () => {
+    const emailInput = debugElement.nativeElement.querySelector(
+      'input[type="email"]'
+    );
+    const passwordInput = debugElement.nativeElement.querySelector(
+      'input[type="password"]'
+    );
+    const submitButton = debugElement.nativeElement.querySelector(
+      'button[type="submit"]'
+    );
+
+    emailInput.value = '';
+    emailInput.dispatchEvent(new Event('input'));
+
+    passwordInput.value = '';
+    passwordInput.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    expect(component.loginForm.valid).toBeFalsy();
+    expect(submitButton.disabled).toBeTruthy();
   });
 });
--- src/app/authentication/login-form/login-form.component.ts
+++ src/app/authentication/login-form/login-form.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+  styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent implements OnInit {
+  loginForm: FormGroup;
+  emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(this.emailPattern),
+      ]),
+      password: new FormControl('', Validators.required),
+    });
   }
+
+  get formControl(): any {
+    return this.loginForm.controls;
+  }
+
+  ngOnInit(): void {}
 
 }
