1f54cd207269b9ebe0a7879cd990a20d541d8874
hamzahsn <amdouni.hamza@hotmail.fr>
2021-02-11 03:23:24 +0100

    Call token interceptor and error interceptor in the core module.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { CookieService } from 'ngx-cookie-service';
 
 import { ApiService } from './services';
+import { ErrorInterceptor, TokenInterceptor } from './interceptors';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, HttpClientModule],
-  providers: [ApiService, CookieService],
+  providers: [
+    ApiService,
+    CookieService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true,
+    },
+  ],
 })
 export class CoreModule {}
