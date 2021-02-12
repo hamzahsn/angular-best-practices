ee26e298be71552060a8b54ecc38aa2a1e73e225
hamzahsn <amdouni.hamza@hotmail.fr>
2021-02-12 01:28:48 +0100

    Call shipments card and details

--- src/app/shipments/shipments.module.ts
+++ src/app/shipments/shipments.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+
 import { ShipmentsComponent } from './shipments.component';
-
-
+import { ShipmentsCardComponent } from './shipments-card/shipments-card.component';
+import { ShipmentsDetailsComponent } from './shipments-details/shipments-details.component';
 
 @NgModule({
-  declarations: [ShipmentsComponent],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    ShipmentsComponent,
+    ShipmentsCardComponent,
+    ShipmentsDetailsComponent,
+  ],
+  imports: [CommonModule, MatCardModule, MatTableModule],
 })
-export class ShipmentsModule { }
+export class ShipmentsModule {}
