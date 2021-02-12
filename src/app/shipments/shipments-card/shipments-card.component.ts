a1e9ffb8237f715089ef2955283b0ad4d24be6bd
hamzahsn <amdouni.hamza@hotmail.fr>
2021-02-12 23:17:45 +0100

    Add title and shipmentsCount as inputs

--- src/app/shipments/shipments-card/shipments-card.component.ts
+++ src/app/shipments/shipments-card/shipments-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-shipments-card',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shipments-card.component.scss'],
 })
 export class ShipmentsCardComponent implements OnInit {
+  @Input()
+  title = '';
+
+  @Input()
+  shipmentsCount = 0;
+
   constructor() {}
 
   ngOnInit(): void {}
