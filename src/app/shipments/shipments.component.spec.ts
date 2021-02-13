dba66a4e5b16afc0a4a8e21eaff8f79b161e2e67
hamzahsn <amdouni.hamza@hotmail.fr>
2021-02-13 01:39:45 +0100

    Calculate the total number of shipments
    Count and display the total shipments made yesterday, in week and in month.

--- src/app/shipments/shipments.component.spec.ts
+++ src/app/shipments/shipments.component.spec.ts
@@ -1,6 +1,15 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
 
+import { of } from 'rxjs';
+import { MatTableModule } from '@angular/material/table';
+
+import { Shipment } from 'app/shared/models';
 import { ShipmentsComponent } from './shipments.component';
+import { ShipmentsService } from 'app/core/services';
+import { ShipmentsDetailsComponent } from './shipments-details/shipments-details.component';
+import { ShipmentsCardComponent } from './shipments-card/shipments-card.component';
 
 describe('ShipmentsComponent', () => {
   let component: ShipmentsComponent;
@@ -8,9 +17,14 @@ describe('ShipmentsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ShipmentsComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        ShipmentsComponent,
+        ShipmentsDetailsComponent,
+        ShipmentsCardComponent,
+      ],
+      imports: [HttpClientTestingModule, MatTableModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -22,4 +36,50 @@ describe('ShipmentsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should get the date of today', () => {
+    const today = new Date();
+    const result = component.getDateFrom(0);
+
+    expect(today.toString()).toEqual(result.toString());
+  });
+
+  it('should get the date of the last 2 days', () => {
+    jasmine.clock().mockDate(new Date('2020-10-10'));
+    const difference = -2;
+    const expectedDate = new Date('2020-10-08');
+    const result = component.getDateFrom(difference);
+
+    expect(new Date(expectedDate).toString()).toEqual(result.toString());
+  });
+
+  it('should count the shipments correctly', inject(
+    [ShipmentsService],
+    (shipmentsService: ShipmentsService) => {
+      jasmine.clock().mockDate(new Date('2020-10-10'));
+      const dataMock = [
+        // shipments made in month
+        { planned_eta: '2020-09-23' },
+        { planned_eta: '2020-10-01' },
+        { planned_eta: '2020-10-01' },
+        // shipments made in 7 days
+        { planned_eta: '2020-10-05' },
+        { planned_eta: '2020-10-04' },
+        { planned_eta: '2020-10-03' },
+        { planned_eta: '2020-10-10' },
+        // shipments made yesterday
+        { planned_eta: '2020-10-09' },
+        { planned_eta: '2020-10-09' },
+      ] as Shipment[];
+
+      spyOn(shipmentsService, 'list').and.returnValue(of(dataMock));
+
+      component.ngOnInit();
+      fixture.detectChanges();
+
+      expect(component.yesterdayCount).toEqual(2);
+      expect(component.lastWeekCount).toEqual(6);
+      expect(component.lastMonthCount).toEqual(9);
+    }
+  ));
 });
--- src/app/shipments/shipments.component.ts
+++ src/app/shipments/shipments.component.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
+import { ShipmentsService } from 'app/core/services';
+import { Shipment } from 'app/shared/models';
 
 @Component({
   selector: 'app-shipments',
   templateUrl: './shipments.component.html',
-  styleUrls: ['./shipments.component.scss']
+  styleUrls: ['./shipments.component.scss'],
 })
-export class ShipmentsComponent implements OnInit {
+export class ShipmentsComponent implements OnInit, OnDestroy {
+  destroy$ = new Subject<void>();
+  shipmentsData: Shipment[] = [];
+  yesterdayCount = 0;
+  lastWeekCount = 0;
+  lastMonthCount = 0;
 
-  constructor() { }
+  constructor(private shipmentsService: ShipmentsService) {}
 
   ngOnInit(): void {
+    this.fetchShipments();
   }
 
+  fetchShipments(): void {
+    this.shipmentsService
+      .list()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data) => {
+        this.shipmentsData = data;
+        this.countShipments(data);
+      });
+  }
+
+  countShipments(data: Shipment[]): void {
+    const today = new Date();
+    const yesterday = this.getDateFrom(-1);
+    const lastWeek = this.getDateFrom(-7);
+    const lastMonth = this.getDateFrom(-30);
+    const counter = { yesterday: 0, week: 0, month: 0 };
+
+    data.forEach(({ planned_eta }) => {
+      const estimatedArrivalTime = new Date(planned_eta);
+
+      if (+estimatedArrivalTime === +yesterday) {
+        counter.yesterday++;
+      }
+
+      if (estimatedArrivalTime <= today) {
+        if (estimatedArrivalTime >= lastWeek) {
+          counter.week++;
+        }
+
+        if (estimatedArrivalTime >= lastMonth) {
+          counter.month++;
+        }
+      }
+    });
+
+    this.yesterdayCount = counter.yesterday;
+    this.lastWeekCount = counter.week;
+    this.lastMonthCount = counter.month;
+  }
+
+  getDateFrom(daysDifference: number): Date {
+    const date = new Date();
+
+    date.setDate(date.getDate() + daysDifference);
+    return new Date(date);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
